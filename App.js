87ad92578d939a08d4d8b5ac6accee8508708cc2
Thomas Klemm <github@tklemm.eu>
2018-07-22 22:06:34 +0200

    Refactor views into two different components
    - CameraView
    - SnapView

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
 import React from "react";
 import {
+  Alert,
+  ImageBackground,
   Text,
-  View,
   TouchableOpacity,
   SafeAreaView,
-  ImageBackground,
-  Alert
+  StyleSheet,
+  View
 } from "react-native";
 import { Camera, Permissions } from "expo";
 
 export default class App extends React.Component {
   state = {
-    hasCameraPermission: null,
-    cameraType: Camera.Constants.Type.back,
-    picture: null
+    snap: null
+  };
+
+  takeSnap = async camera => {
+    const snap = await camera.takePictureAsync();
+    this.setState({ snap: snap });
+  };
+
+  clearSnap = () => {
+    this.setState({ snap: null });
+  };
+
+  submitSnap = () => {
+    Alert.alert(
+      "Submitting snap",
+      "We're scanning your photo for some fruit. Please wait..."
+    );
+  };
+
+  render() {
+    const { snap } = this.state;
+    if (snap) {
+      return (
+        <SnapView
+          snap={snap}
+          onSubmitSnap={this.submitSnap}
+          onClearSnap={this.clearSnap}
+        />
+      );
+    } else {
+      return <CameraView onTakeSnap={this.takeSnap} />;
+    }
+  }
+}
+
+class CameraView extends React.Component {
+  state = {
+    hasCameraPermission: null
   };
 
   async componentWillMount() {
@@ -21,84 +57,23 @@ export default class App extends React.Component {
     this.setState({ hasCameraPermission: status === "granted" });
   }
 
-  toggleCameraType() {
-    const newCameraType =
-      this.state.cameraType == Camera.Constants.Type.back
-        ? Camera.Constants.Type.front
-        : Camera.Constants.Type.back;
-    this.setState({ cameraType: newCameraType });
-  }
-
-  async snapPicture() {
-    if (this.camera) {
-      const picture = await this.camera.takePictureAsync();
-      this.setState({ picture: picture });
-    }
-  }
-
-  clearPicture() {
-    this.setState({ picture: null });
-  }
-
-  performScan() {
-    Alert.alert("Scanning", "Scanning your photo for fruits. Please wait...");
-  }
-
   render() {
-    const { picture, hasCameraPermission } = this.state;
-    if (picture !== null) {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
       return (
-        <View style={{ flex: 1 }}>
-          <ImageBackground
-            source={picture}
-            style={{ width: "100%", height: "100%" }}
-          >
-            <SafeAreaView style={{ flex: 1 }}>
-              <View
-                style={{
-                  flex: 1,
-                  flexDirection: "row",
-                  justifyContent: "space-between"
-                }}
-              >
-                <TouchableOpacity
-                  style={{
-                    flex: 1,
-                    alignSelf: "flex-end",
-                    alignItems: "center"
-                  }}
-                  onPress={() => this.clearPicture()}
-                >
-                  <Text
-                    style={{ fontSize: 24, fontWeight: "bold", color: "white" }}
-                  >
-                    Retake
-                  </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={{
-                    flex: 1,
-                    alignSelf: "flex-end",
-                    alignItems: "center"
-                  }}
-                  onPress={() => this.performScan()}
-                >
-                  <Text
-                    style={{ fontSize: 24, fontWeight: "bold", color: "white" }}
-                  >
-                    Submit
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </SafeAreaView>
-          </ImageBackground>
-        </View>
+        <Text>
+          Waiting for camera access. You should see a dialog asking for camera
+          permissions in a moment...
+        </Text>
       );
-    } else if (hasCameraPermission === null) {
-      return <Text>Waiting for access to camera.</Text>;
     } else if (hasCameraPermission === false) {
-      return <Text>No access to camera.</Text>;
+      return (
+        <Text>
+          Camera access doesn't seem to be granted. Please allow camera access..
+        </Text>
+      );
     } else {
+      const { onTakeSnap } = this.props;
       return (
         <View style={{ flex: 1 }}>
           <Camera
@@ -109,40 +84,12 @@ export default class App extends React.Component {
             }}
           >
             <SafeAreaView style={{ flex: 1 }}>
-              <View
-                style={{
-                  flex: 1,
-                  backgroundColor: "transparent",
-                  flexDirection: "row"
-                }}
-              >
+              <View style={styles.buttonRow}>
                 <TouchableOpacity
-                  style={{
-                    flex: 1,
-                    alignSelf: "flex-end",
-                    alignItems: "center"
-                  }}
-                  onPress={() => this.toggleCameraType()}
+                  style={styles.buttonElement}
+                  onPress={() => onTakeSnap(this.camera)}
                 >
-                  <Text
-                    style={{ fontSize: 24, fontWeight: "bold", color: "white" }}
-                  >
-                    Flip
-                  </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={{
-                    flex: 1,
-                    alignSelf: "flex-end",
-                    alignItems: "center"
-                  }}
-                  onPress={() => this.snapPicture()}
-                >
-                  <Text
-                    style={{ fontSize: 24, fontWeight: "bold", color: "white" }}
-                  >
-                    Snap
-                  </Text>
+                  <Text style={styles.buttonText}>Snap</Text>
                 </TouchableOpacity>
               </View>
             </SafeAreaView>
@@ -152,3 +99,53 @@ export default class App extends React.Component {
     }
   }
 }
+
+const SnapView = ({ snap, onSubmitSnap, onClearSnap }) => {
+  return (
+    <View style={{ flex: 1 }}>
+      <ImageBackground source={snap} style={{ width: "100%", height: "100%" }}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <View style={styles.buttonRow}>
+            <TouchableOpacity
+              style={styles.buttonElement}
+              onPress={onClearSnap}
+            >
+              <Text style={styles.buttonText}>Retake</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.buttonElement}
+              onPress={onSubmitSnap}
+            >
+              <Text style={styles.buttonText}>Submit</Text>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  headline: {
+    fontSize: 24,
+    color: "black"
+  },
+  buttonRow: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row"
+  },
+  buttonElement: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    paddingTop: 16,
+    paddingBottom: 16
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white"
+  }
+});
