ac58236e3ea48162d3b346df7ce7982ebe7b698d
Thomas Klemm <github@tklemm.eu>
2018-07-22 19:15:45 +0200

    Basic camera integration using Expo
    The camera component from expo is used to ask for camera permissions, then show the image from the back camera
    with a flip option for the front camera.

--- App.js
+++ App.js
@@ -1,23 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, TouchableOpacity, SafeAreaView } from "react-native";
+import { Camera, Permissions } from "expo";
 
 export default class App extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    cameraType: Camera.Constants.Type.back
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === "granted" });
+  }
+
+  toggleCameraType() {
+    const newCameraType =
+      this.state.cameraType == Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back;
+    this.setState({ cameraType: newCameraType });
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <Text>Waiting for access to camera.</Text>;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera.</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.cameraType}>
+            <SafeAreaView style={{ flex: 1 }}>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: "transparent",
+                  flexDirection: "row"
+                }}
+              >
+                <TouchableOpacity
+                  style={{
+                    flex: 1,
+                    alignSelf: "flex-end",
+                    alignItems: "center"
+                  }}
+                  onPress={() => this.toggleCameraType()}
+                >
+                  <Text
+                    style={{ fontSize: 24, fontWeight: "bold", color: "white" }}
+                  >
+                    Flip
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </SafeAreaView>
+          </Camera>
+        </View>
+      );
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
